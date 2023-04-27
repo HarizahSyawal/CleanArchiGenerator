3fc58f28fad7ee92d6a7f07d524d1a4e80bb7965
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-04-27 14:25:35 +0700

    Test Generate Architecture
    Ex : dart .\generator.dart feature login
    dart .\generator.dart feature ......

---
+++ cleanarchigen/README.md
@@ -0,0 +1,3 @@
+# cleanarchigen
+
+A new Flutter project.
---
+++ cleanarchigen/android/app/src/main/kotlin/com/example/cleanarchigen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cleanarchigen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
